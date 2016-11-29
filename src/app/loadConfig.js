4f5542421a3a6d546d8d2130a8fa62d5f00973c6
feliun <felipe.polo@gmail.com>
2016-11-29 17:57:03 +0000

    Using systemic interface

---
+++ src/app/loadConfig.js
@@ -0,0 +1,17 @@
+import Confabulous from 'confabulous';
+
+const { loaders } = Confabulous;
+
+export default () => {
+  const start = (cb) => {
+    new Confabulous()
+    .add(() => loaders.require({ path: './config/default.json', watch: true }))
+    .add(() => loaders.require({ path: `./config/${process.env.NODE_ENV}.json`, watch: true }))
+    .add(() => loaders.args())
+    .end(cb);
+  };
+
+  const stop = () => {};
+
+  return { start, stop };
+};
---
+++ src/app/loadLogger.js
@@ -0,0 +1,15 @@
+import prepper from 'prepper';
+
+export default () => {
+  const start = (cb) => {
+    const logger = new prepper.Logger().on('message', (event) => {
+      // TODO: Replace with your logger of choice
+      console.log(event.level.toUpperCase(), event.message);
+    });
+    cb(null, logger);
+  };
+
+  const stop = () => {};
+
+  return { start, stop };
+};
